e1731a134871807afea71b21ce80b4fad043c433
Kat <120306573+KatGia@users.noreply.github.com>
2025-05-18 19:07:56 +0200

    Add files via upload

---
+++ basic-commands.sql
@@ -0,0 +1,31 @@
+### 1. Creating Tables
+
+CREATE TABLE pazienti (
+    id_paziente INTEGER PRIMARY KEY,
+    nome TEXT,
+    eta INTEGER,
+    sesso TEXT,
+    malattia TEXT
+);
+
+CREATE TABLE campioni (
+    id_campione INTEGER PRIMARY KEY,
+    id_paziente INTEGER,
+    tipo_campione TEXT,
+    data_raccolta DATE,
+    FOREIGN KEY (id_paziente) REFERENCES pazienti(id_paziente)
+);
+
+
+### 2. Inserting Data
+
+INSERT INTO pazienti (id_paziente, nome, eta, sesso, malattia) VALUES
+(1, 'Anna Rossi', 67, 'F', 'Adenocarcinoma gastrico'),
+(2, 'Luca Bianchi', 55, 'M', 'Psoriasi'),
+(3, 'Maria Verdi', 72, 'F', 'Adenocarcinoma gastrico');
+
+INSERT INTO campioni (id_campione, id_paziente, tipo_campione, data_raccolta) VALUES
+(101, 1, 'RNA-seq', '2024-04-12'),
+(102, 2, 'siero', '2024-03-10'),
+(103, 3, 'RNA-seq', '2024-05-02');
+
